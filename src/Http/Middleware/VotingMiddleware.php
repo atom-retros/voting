7fe313a276fb86e0e2d204ce6685a0a6fb2d98ac
kani_robinson <kani.robinson@mvfglobal.com>
2024-07-05 23:40:03 +0100

    Created Middleware for handling FindRetros Requests

---
+++ src/Http/Middleware/VotingMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Atom\Voting\Http\Middleware;
+
+use Atom\Voting\Services\FindRetrosService;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class VotingMiddleware
+{
+    /**
+     * Handle an incoming request.
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $service = app(FindRetrosService::class);
+
+        if (! config('voting.enabled', false)) {
+            return $next($request);
+        }
+
+        if (! $service->voted()) {
+            return redirect()->to($service->getRedirectUri());
+        }
+
+        return $next($request);
+    }
+}
